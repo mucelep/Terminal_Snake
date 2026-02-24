187f3b8d53b5eab960283118f1917b85f89502e3
mucelep <celepm82@gmail.com>
2026-02-24 22:18:40 +0300

    almost done

--- terminalsnake.c
+++ terminalsnake.c
@@ -85,17 +85,17 @@ struct direction check_way(char c)
 		dir.dx = 1; // sağa doğru 1 hareket
 		dir.dy = 0;
 	}
-		if(c == 'a')
+	else if(c == 'a')
 	{
 		dir.dx = -1; //sola doğru 1 hareket
 		dir.dy = 0;
 	}
-		if(c == 's')
+	else if(c == 's')
 	{
 		dir.dx = 0;
 		dir.dy = 1; // assagı dogru 1 hareket
 	}
-		if(c == 'w')
+	else if(c == 'w')
 	{
 		dir.dx = 0; // yukarı dogru 1 hareket
 		dir.dy = -1;
@@ -108,6 +108,10 @@ int main()
 	t_segment snake[MAX_LENGTH];
 	snake[0].x = 5;
 	snake[0].y = 5;
+	snake[1].x = 4;
+	snake[1].y = 5;
+	snake[2].x = 3;
+	snake[2].y = 5;
 	int length = 3;
 	char c;
 	char choise;
@@ -142,7 +146,20 @@ int main()
 			snake[0].y += d.dy;
 			snake[0].x = wallcheck_x(snake[0].x);
 			snake[0].y = wallcheck_y(snake[0].y);
-			if (snake[0].x == food_x && snake[0].y == food_y)
+			int	hit = 0;
+			for (int i = 1; i < length; i++) // kendine çarpma kontrolü
+			{
+				if (snake[0].x == snake[i].x && snake[0].y == snake[i].y)
+					hit = 1;
+			}
+			if (hit)
+			{
+				disenable_raw_mode(&original);
+				printf("\033[H\033[J");
+				printf("GAME OVER ! YOU BİTE YOURSELF :/ SCORE = %d", length - 1);
+				return (0);
+			}
+			if (snake[0].x == food_x && snake[0].y == food_y)//yem kontrolü
 			{
 				length++;
 				food_x = rand() % (WIDTH - 2) + 2;
@@ -157,9 +174,61 @@ int main()
 	}
 	else if(choise == '2')
 	{
-		
+		enable_raw_mode(&original);
+		while (read(0, &c, 1) > 0);
+		while (1)
+		{
+			clear_screen();
+			draw_board(snake,length,food_x,food_y);
+			int n = read(0, &c, 1);
+			if (n > 0)
+			{
+				if(c == 'q')
+					break;
+				d = check_way(c);
+			}
+			for(int i = length - 1; i > 0; i--)
+			{
+				snake[i].x = snake[i - 1].x;
+				snake[i].y = snake[i - 1].y;
+			}
+			snake[0].x += d.dx;
+			snake[0].y += d.dy;
+			if (snake[0].x <= 1 || snake[0].x >= WIDTH ||
+				snake[0].y <= 1 || snake[0].y >= HEIGHT )
+			{
+				disenable_raw_mode(&original);
+				printf("\033[H\033[J");
+				printf("GAME OVER! YOU HİT THE WALL :/ Score: %d\n", length - 1);
+				return (0);
+			}
+			int	hit = 0;
+			for (int i = 1; i < length; i++) // kendine çarpma kontrolü
+			{
+				if (snake[0].x == snake[i].x && snake[0].y == snake[i].y)
+					hit = 1;
+			}
+			if (hit)
+			{
+				disenable_raw_mode(&original);
+				printf("\033[H\033[J");
+				printf("GAME OVER ! YOU BİTE YOURSELF :/ SCORE = %d", length - 1);
+				return (0);
+			}
+			if (snake[0].x == food_x && snake[0].y == food_y)//yem kontrolü
+			{
+				length++;
+				food_x = rand() % (WIDTH - 2) + 2;
+				food_y = rand() % (HEIGHT - 2) + 2;
+			}
+			if(d.dy != 0)
+				usleep(170000);
+			else
+				usleep(100000);
+		}
+		disenable_raw_mode(&original);
 	}
 	else
 		printf("WRONG CHOİSE !!");
-	return 0;
+	return (0);
 }
